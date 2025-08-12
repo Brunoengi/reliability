0958cea77a1d2af9048e2599997779dd13b0cd07
Bruno Santos <bruno.if@hotmail.com>
2025-08-12 15:30:47 -0300

    add correlated examples

---
+++ examples/articles/01_generic_uncorrelated/1.24_mc.py
@@ -0,0 +1,22 @@
+from main import *
+import numpy as np
+
+## Expected β = 2.35
+
+def gfunction(x, d):
+    x1 = x[0]
+    x2 = x[1]
+    g = np.exp(0.2 * x1 + 6.2) - np.exp(0.47 * x2 + 5.0)
+    return g
+
+xvar = [
+    {'varname': 'x1', 'vardist': 'normal', 'varmean': 0.0, 'varstd': 1.0},
+    {'varname': 'x2', 'vardist': 'normal', 'varmean': 0.0, 'varstd': 1.0},
+]
+
+dvar = [
+    {'varname': 'factor1', 'varvalue': 1.00},
+]
+
+example = Reliability(xvar, dvar, gfunction)
+example.mc(1000, 5000, 0.01)
